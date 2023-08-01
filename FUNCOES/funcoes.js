90c679402c92343ccc410c6d9e4dfd3589d969e5
Giovanni Caramello <100931407+iamGiovanniCaramelloGrego@users.noreply.github.com>
2023-08-01 12:42:56 -0300

    Add files via upload

---
+++ FUNCOES/funcoes.js
@@ -0,0 +1,11 @@
+
+function dadosacesso(){
+    meucartao = prompt("digite o numero do cartão: ")
+    senha= prompt("digite a senha do seu cartão: ")
+}
+
+function saque(valor){
+    saldo = 1000
+    saldo = saldo - valor
+    return saldo
+}
