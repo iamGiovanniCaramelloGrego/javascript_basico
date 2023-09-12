45d7aba6b2cab94a449b14519a1cd3281c108e5d
Giovanni Caramello <100931407+iamGiovanniCaramelloGrego@users.noreply.github.com>
2023-09-12 10:44:25 -0300

    CADASTRO

---
+++ AULA1/funcao.js
@@ -0,0 +1,29 @@
+var nome
+var email
+var num = 0
+saldo = 1000
+var saque = 0
+var deposito = 0
+
+ function sacar(saque){
+    saldo = saldo - saque
+    return saldo
+ }
+
+ function depositar(deposito){
+    saldo = saldo + deposito
+    return saldo
+ }
+
+
+ function escrever(quadro) {
+    info = "Seja bem vindo: ";
+    info = "Usuário número " + (++num) + "\n";
+    info += "Nome: " + nome + "\n";
+    info += "E-mail: " + email + "\n \n";;
+    info += "Saldo inicial: " + saldo + "\n";
+    info += "Saldo após sacar:" + sacar(saque) + "\n";
+    info += "Saldo após depositar:" + depositar(deposito) + "\n \n";
+
+    quadro.value += info;
+ }
